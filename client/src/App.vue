4df003c5d6f1075c7247a343cbb63cddb4022615
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-04 09:06:29 +0100

    Title of the page has been made as a script in App.vue file

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,19 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
+<script>
+export default {
+  data() {
+    return {
+      title: 'Crypto Dive'
+    }
+  },
+  mounted() {
+    document.title = this.title;
+  }
+}
+</script>
+
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
