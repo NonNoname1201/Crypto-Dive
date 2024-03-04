3fb33fa6b23cab8b23e928ff7d786e69e2406473
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-04 13:34:34 +0100

    Deletion of unused autogenerated files and unused autogenerated code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <script>
@@ -21,12 +20,10 @@ export default {
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
