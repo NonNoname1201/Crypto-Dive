dfb09a7b50db88f1a13bb2535926e15fd9c16dd2
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-05 17:49:26 +0100

    Addition of structure components of second page

---
+++ client/src/components/page_second/Content.vue
@@ -0,0 +1,59 @@
+<script setup>
+import IconCore from "@/components/icons/page_second/IconCore.vue";
+import IconNews from "@/components/icons/page_second/IconNews.vue";
+import IconMoney from "@/components/icons/page_second/IconMoney.vue";
+import IconMember from "@/components/icons/page_second/IconMember.vue";
+import IconStudent from "@/components/icons/page_second/IconStudent.vue";
+</script>
+
+<template>
+<div class="Content">
+  <div class="core"><IconCore/></div>
+  <div class="images">
+    <IconNews/>
+    <IconMoney/>
+    <IconMember/>
+    <IconStudent/>
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+.Content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80vh;
+  z-index: -1;
+}
+
+.core {
+  margin: 0 auto;
+  width: 60%;
+  height: 100%;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.images {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 160px;
+  width: 79%;
+  height: 90%;
+  margin: 0;
+  svg {
+    width: 100%;
+    height: 100%;
+    background: transparent;
+  }
+
+  position: absolute;
+  background: transparent;
+}
+</style>
---
+++ client/src/components/page_second/Header.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<template>
+  <div class="header">
+      <p class="pageName" ref="pageName">Nasz ekosystem</p>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+}
+
+.pageName {
+  font-size: 50px;
+  font-weight: 800;
+  line-height: 1.3;
+
+  padding-bottom: 8px;
+  background: linear-gradient(270deg, #BD00FF 0%, #0047FF 53%, #31FA0B 100%);
+  border: 0;
+  position: relative;
+  z-index: 1;
+}
+
+.pageName:before {
+  content: "";
+  display: block;
+  height: 69px;
+  width: 100%;
+  background: black;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+
+</style>
