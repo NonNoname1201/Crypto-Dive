57d45ff48c5374fec4546feddff8c5e08f20a1be
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-05 18:04:00 +0100

    Working App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,13 +2,7 @@
 import PageFirst from '@/components/PageFirst.vue';
 import PageSecond from '@/components/PageSecond.vue';
 import PageThird from '@/components/PageThird.vue';
-import { defineComponent } from 'vue';
 
-const components = {
-  FirstPage,
-  SecondPage,
-  ThirdPage,
-};
 </script>
 
 <script>
@@ -43,23 +37,5 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
