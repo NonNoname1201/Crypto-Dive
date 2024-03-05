4076510c5caff0c38829e28a3384977efc1b6f61
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-05 17:50:52 +0100

    Rework of PageSecond.vue using structured .vue files

--- client/src/components/PageSecond.vue
+++ client/src/components/PageSecond.vue
@@ -1,11 +1,19 @@
 <script setup>
-
+import Header from "@/components/page_second/Header.vue";
+import Content from "@/components/page_second/Content.vue";
 </script>
 
 <template>
-<h2>Nasz ekosystem</h2>
+  <div class="Page-second page">
+    <Header />
+    <Content />
+  </div>
 </template>
 
 <style scoped>
+.Page-second {
+  max-height: 100vh;
+  min-height: 100vh;
+}
 
 </style>
