12d58b5df5b8ccb72b504fb27a3124fab44cbcc6
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-07 19:37:59 +0100

    Now thin page uses new svg's

--- client/src/components/page_second/PageThin.vue
+++ client/src/components/page_second/PageThin.vue
@@ -1,28 +1,10 @@
 <script setup>
 import SkeletonThin from "@/components/icons/page_second/thin/SkeletonThin.vue";
-import IconMemberThin from "@/components/icons/page_second/thin/IconMemberThin.vue";
-import IconNewsThin from "@/components/icons/page_second/thin/IconNewsThin.vue";
-import IconMoneyThin from "@/components/icons/page_second/thin/IconMoneyThin.vue";
-import IconStudentThin from "@/components/icons/page_second/thin/IconStudentThin.vue";
 </script>
 
 <template>
   <div class="Content">
     <div class="skeleton-thin"><SkeletonThin/></div>
-    <div class="icon-container-thin">
-      <div class="icon-thin icon-news-thin">
-        <IconNewsThin/>
-      </div>
-      <div class="icon-thin icon-money-thin">
-        <IconMoneyThin/>
-      </div>
-      <div class="icon-thin icon-member-thin">
-        <IconMemberThin/>
-      </div>
-      <div class="icon-thin icon-student-thin">
-        <IconStudentThin/>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -71,34 +53,6 @@ import IconStudentThin from "@/components/icons/page_second/thin/IconStudentThin
     background: transparent;
   }
   background: transparent;
-}
-
-.icon-news-thin{
-  --svg-size: 60%;
-  width: var(--svg-size);
-  height: var(--svg-size);
-  top: -6%;
-}
-
-.icon-money-thin{
-  --svg-size: 65%;
-  width: var(--svg-size);
-  height: var(--svg-size);
-  top: 10%;
-}
-
-.icon-member-thin{
-  --svg-size: 53%;
-  width: var(--svg-size);
-  height: var(--svg-size);
-  bottom: 12%;
-}
-
-.icon-student-thin{
-  --svg-size: 79%;
-  width: var(--svg-size);
-  height: var(--svg-size);
-  bottom: -20%;
 }
 
 
