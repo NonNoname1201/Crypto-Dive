aa3f7101cea8e4182e143b8246bb25ed373ccfa4
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-08 18:42:20 +0100

    Text in header of second page is now centered

--- client/src/components/page_second/Header.vue
+++ client/src/components/page_second/Header.vue
@@ -3,27 +3,28 @@
 </script>
 
 <template>
-  <div class="header">
-      <p class="pageName" ref="pageName">Nasz ekosystem</p>
-  </div>
+    <div class="header">
+        <p class="pageName" ref="pageName">Nasz ekosystem</p>
+    </div>
 </template>
 
 <style scoped>
 .header {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 60px;
 }
 
 .pageName {
-  font-size: 50px;
-  font-weight: 700;
-  line-height: 61px;
-  padding-bottom: 14px;
+    font-size: 50px;
+    font-weight: 700;
+    line-height: 61px;
+    padding-bottom: 14px;
+    text-align: center;
 
-  background: linear-gradient(black, black) padding-box, linear-gradient(270deg, #BD00FF 0%, #0047FF 53%, #31FA0B 100%) border-box;
-  border-bottom: 6px solid transparent;
+    background: linear-gradient(black, black) padding-box, linear-gradient(270deg, #BD00FF 0%, #0047FF 53%, #31FA0B 100%) border-box;
+    border-bottom: 6px solid transparent;
 }
 
 </style>
