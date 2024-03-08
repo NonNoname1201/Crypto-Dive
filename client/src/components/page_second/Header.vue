960705977cdf0a5641bc2935736241a802fcae5f
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-08 18:34:11 +0100

    Optimized line in header of second page

--- client/src/components/page_second/Header.vue
+++ client/src/components/page_second/Header.vue
@@ -2,17 +2,6 @@
 
 </script>
 
-<script>
-export default {
-  data() {
-    return {
-    }
-  },
-  mounted() {
-  }
-}
-</script>
-
 <template>
   <div class="header">
       <p class="pageName" ref="pageName">Nasz ekosystem</p>
@@ -31,24 +20,10 @@ export default {
   font-size: 50px;
   font-weight: 700;
   line-height: 61px;
-
   padding-bottom: 14px;
-  background: linear-gradient(270deg, #BD00FF 0%, #0047FF 53%, #31FA0B 100%);
-  border: 0;
-  position: relative;
-  z-index: 1;
-}
 
-.pageName:before {
-  content: "";
-  display: block;
-  height: 69px;
-  width: 100%;
-  background: black;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
+  background: linear-gradient(black, black) padding-box, linear-gradient(270deg, #BD00FF 0%, #0047FF 53%, #31FA0B 100%) border-box;
+  border-bottom: 6px solid transparent;
 }
 
 </style>
