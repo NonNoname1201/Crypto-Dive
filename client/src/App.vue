8d9729b725cb67fa24008b8232b6c2ed3d9a92d8
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-14 12:45:18 +0100

    Added proper background

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,41 +1,25 @@
 <script setup>
+import {onMounted} from 'vue';
 import PageFirst from '@/components/PageFirst.vue';
 import PageSecond from '@/components/PageSecond.vue';
 import PageThird from '@/components/PageThird.vue';
 
-</script>
-
-<script>
-export default {
-  data() {
-    return {
-      title: 'Crypto Dive'
-    }
-  },
-  mounted() {
-    document.title = this.title;
-  }
-}
+onMounted(() => {
+    document.title = "Crypto Dive";
+});
 </script>
 
 <template>
-  <header>
-  </header>
-
-  <main>
-    <PageFirst />
-    <PageSecond />
-    <PageThird />
-  </main>
+    <main>
+        <PageFirst/>
+        <PageSecond/>
+        <PageThird/>
+    </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+main {
+    background-image: url("./assets/tile.png");
+    background-repeat: repeat;
 }
 </style>
