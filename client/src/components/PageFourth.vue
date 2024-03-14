00520a555cc83cbc8222066403e78f8313a0a849
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-14 15:17:59 +0100

    Removal of unused content of second page, cleanup

---
+++ client/src/components/PageFourth.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="Page-fourth page">
+    <h1>Page Fourth</h1>
+  </div>
+</template>
+
+<style scoped>
+  .Page-fourth {
+    min-height: 100vh;
+  }
+</style>
